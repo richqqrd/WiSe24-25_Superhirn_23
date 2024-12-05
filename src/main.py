6954af63e035362a07efdbe657680f9ffaca617a
richqqrd <spam@richardmiddendorf.de>
2024-12-05 17:44:16 +0100

    test

--- src/main.py
+++ src/main.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     # Beispielausführung
     print("Addition: 2 + 3 =", add(2, 3))
     print("Subtraktion: 5 - 2 =", subtract(5, 2))
+    print("Subtraktion: 5 - 2 =", subtract(5, 2))
 
