7f1878acfa5bbf03c6e50e8680d3d0915f5855e6
richqqrd <spam@richardmiddendorf.de>
2024-12-05 17:58:22 +0100

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,6 +13,6 @@ def test_subtract():
     """Testet die Funktion subtract."""
     assert subtract(5, 2) == 3
     assert subtract(0, 5) == -5
-    assert subtract(0, 0) == 0
+    assert subtract(0, 0) == 1
 if __name__ == "__main__":
     pytest.main()
