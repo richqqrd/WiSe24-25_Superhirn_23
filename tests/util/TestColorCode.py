5e24aef39aa284fda7a9ca5a18fad421d40cc38f
richqqrd <spam@richardmiddendorf.de>
2024-12-07 14:09:09 +0100

    fixed testClass for ColorCode

--- tests/util/TestColorCode.py
+++ tests/util/TestColorCode.py
@@ -1,19 +1,5 @@
 import pytest
-from enum import Enum
-
-class ColorCode(Enum):
-    """
-    Enum representing various color codes with their respective integer values.
-    Provides methods to check if a color is primary or has a rating.
-    """
-    RED = 1
-    GREEN = 2
-    YELLOW = 3
-    BLUE = 4
-    ORANGE = 5
-    BROWN = 6
-    WHITE = 7
-    BLACK = 8
+from src.util.ColorCode import ColorCode
 
 def test_enum_values():
     """
