ba6fe87c327e53edbb4bb971e478fee4d35e4c6a
richqqrd <spam@richardmiddendorf.de>
2024-12-07 15:56:03 +0100

    updated test_game_renderer.py | with ghamdan

--- tests/CLI/game_renderer/test_game_renderer.py
+++ tests/CLI/game_renderer/test_game_renderer.py
@@ -26,18 +26,22 @@ class TestGameRenderer(unittest.TestCase):
         expected_output = f"{ColorCode.RED.get_ansi_code()}1\033[0m{ColorCode.GREEN.get_ansi_code()}2\033[0m{ColorCode.BLUE.get_ansi_code()}4\033[0m"
         self.assertEqual(colored_output, expected_output)
 
-
     def test_render_game_state(self):
         game_state = {
-            1: ([ColorCode.RED, ColorCode.GREEN, ColorCode.BLUE, ColorCode.ORANGE, ColorCode.BROWN], [FeedbackColorCode.BLACK, FeedbackColorCode.WHITE]),
-            2: ([ColorCode.YELLOW, ColorCode.ORANGE, ColorCode.BROWN, ColorCode.WHITE, ColorCode.BLACK], [FeedbackColorCode.WHITE, FeedbackColorCode.BLACK])
+            1: ([ColorCode.RED, ColorCode.GREEN, ColorCode.BLUE], [FeedbackColorCode.BLACK, FeedbackColorCode.WHITE]),
+            2: (
+            [ColorCode.YELLOW, ColorCode.ORANGE, ColorCode.BROWN], [FeedbackColorCode.WHITE, FeedbackColorCode.BLACK])
         }
         self.renderer.render_game_state(game_state)
         output = self.held_output.getvalue().strip()
         self.assertIn("Super Superhirn", output)
         self.assertIn("Round | Feedback | Guess", output)
-        self.assertIn("1     | \033[90m8\033[0m\033[97m7\033[0m | \033[31m1\033[0m\033[32m2\033[0m\033[34m4\033[0m\033[38;5;214m5\033[0m\033[38;5;94m6\033[0m", output)
-        self.assertIn("2     | \033[97m7\033[0m\033[90m8\033[0m | \033[33m3\033[0m\033[38;5;214m5\033[0m\033[38;5;94m6\033[0m\033[97m7\033[0m\033[90m8\033[0m", output)
+        self.assertIn(
+            f"1     | {FeedbackColorCode.BLACK.get_ansi_code()}8\033[0m{FeedbackColorCode.WHITE.get_ansi_code()}7\033[0m | {ColorCode.RED.get_ansi_code()}1\033[0m{ColorCode.GREEN.get_ansi_code()}2\033[0m{ColorCode.BLUE.get_ansi_code()}4\033[0m",
+            output)
+        self.assertIn(
+            f"2     | {FeedbackColorCode.WHITE.get_ansi_code()}7\033[0m{FeedbackColorCode.BLACK.get_ansi_code()}8\033[0m | {ColorCode.YELLOW.get_ansi_code()}3\033[0m{ColorCode.ORANGE.get_ansi_code()}5\033[0m{ColorCode.BROWN.get_ansi_code()}6\033[0m",
+            output)
 
     def test_render_message(self):
         self.renderer.render_message("Test Message")
