89f34fe3ff7d9e7084bb9d69fd221433dbc82f55
richqqrd <spam@richardmiddendorf.de>
2024-12-23 13:05:54 +0100

    updated PlayerGuesser.py  | with Matthias

--- src/GameLogic/Guesser/PlayerGuesser.py
+++ src/GameLogic/Guesser/PlayerGuesser.py
@@ -2,6 +2,7 @@ from typing import List
 
 from src.GameLogic.Guesser.IGuesser import IGuesser
 from src.util.ColorCode import ColorCode
+from src.util.FeedbackColorCode import FeedbackColorCode
 
 
 class PlayerGuesser(IGuesser):
@@ -13,3 +14,6 @@ class PlayerGuesser(IGuesser):
 
     def set_guess(self, guess: List[ColorCode]):
         self.current_guess = guess
+
+    def process_feedback(self, feedback: List[FeedbackColorCode]) -> None:
+        pass
