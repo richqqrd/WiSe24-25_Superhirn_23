94a0864d9d38969a747e1a7060e4b853fc46a305
richqqrd <spam@richardmiddendorf.de>
2025-01-03 10:24:56 +0100

    updated tests in util | with Marco

--- tests/util/test_FeedbackColorCode.py
+++ tests/util/test_FeedbackColorCode.py
@@ -1,26 +1,38 @@
-# tests/util/test_FeedbackColorCode.py
+"""Test module for FeedbackColorCode enum."""
+
+import unittest
 from src.util.feedback_color_code import FeedbackColorCode
 
 
-def test_enum_values():
-    """
-    Test that the enum values are correctly assigned to the colors.
-    """
-    assert FeedbackColorCode.WHITE.value == 7
-    assert FeedbackColorCode.BLACK.value == 8
+class TestFeedbackColorCode(unittest.TestCase):
+    """Test cases for FeedbackColorCode enum."""
+
+    def test_enum_values(self):
+        """Test that enum values are correctly assigned."""
+        self.assertEqual(FeedbackColorCode.WHITE.value, 7)
+        self.assertEqual(FeedbackColorCode.BLACK.value, 8)
+
+    def test_get_ansi_code(self):
+        """Test ANSI color code generation."""
+        self.assertEqual(FeedbackColorCode.WHITE.get_ansi_code(), "\033[97m")
+        self.assertEqual(FeedbackColorCode.BLACK.get_ansi_code(), "\033[90m")
+
+    def test_str_method(self):
+        """Test string representation."""
+        self.assertEqual(str(FeedbackColorCode.WHITE), "\033[97mWHITE\033[0m")
+        self.assertEqual(str(FeedbackColorCode.BLACK), "\033[90mBLACK\033[0m")
+
+    def test_repr_method(self):
+        """Test repr representation."""
+        self.assertEqual(repr(FeedbackColorCode.WHITE), "<FeedbackColorCode.WHITE: 7>")
+        self.assertEqual(repr(FeedbackColorCode.BLACK), "<FeedbackColorCode.BLACK: 8>")
+
+    def test_equality(self):
+        """Test equality comparison."""
+        self.assertEqual(FeedbackColorCode.WHITE, FeedbackColorCode.WHITE)
+        self.assertEqual(FeedbackColorCode.BLACK, FeedbackColorCode.BLACK)
+        self.assertNotEqual(FeedbackColorCode.WHITE, FeedbackColorCode.BLACK)
 
 
-def test_get_ansi_code():
-    """
-    Test that the get_ansi_code method returns the correct ANSI color code.
-    """
-    assert FeedbackColorCode.WHITE.get_ansi_code() == "\033[97m"
-    assert FeedbackColorCode.BLACK.get_ansi_code() == "\033[90m"
-
-
-def test_str_method():
-    """
-    Test that the __str__ method returns the correct string representation.
-    """
-    assert str(FeedbackColorCode.WHITE) == "\033[97mWHITE\033[0m"
-    assert str(FeedbackColorCode.BLACK) == "\033[90mBLACK\033[0m"
+if __name__ == "__main__":
+    unittest.main()
